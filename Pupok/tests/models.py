ed5e4d0c4cbdb00986bfa1d4a7983b8ebd8fc7b9
Fanaenic <korapetr@gmail.com>
2024-04-19 11:51:08 +0300

    добавление методички

--- Pupok/tests/models.py
+++ Pupok/tests/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    objects = None
+    title = models.CharField('Номер', max_length=50)
+    anons = models.CharField('Сам вопрос', max_length=250)
+    full_text = models.TextField('Варианты ')
+
+    def __str__(self):
+        return self.title
+
+
