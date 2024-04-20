a133a83dcb222321f83366144ea4552df8c04e93
IIcon18 <ogkrit882@gmail.com>
2024-04-20 22:36:01 +0300

    вопросы

--- Pupok/tests/models.py
+++ Pupok/tests/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class Articles(models.Model):
+class Questions(models.Model):
     objects = None
-    title = models.CharField('Номер', max_length=50)
+    number = models.CharField('Номер', max_length=50)
     anons = models.CharField('Сам вопрос', max_length=250)
     full_text = models.TextField('Варианты ')
 
     def __str__(self):
-        return self.title
+        return self.number
 
 
