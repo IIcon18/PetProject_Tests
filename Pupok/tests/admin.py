236ea2e22efa40958add9903b4231f4e98f34b6c
Ilya <145450507+IIcon18@users.noreply.github.com>
2024-05-12 00:17:21 +0300

    Delete Pupok directory
    откат

--- Pupok/tests/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Questions
-
-admin.site.register(Questions)
--- Pupok/tests/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Questions
-
-def tests_home(request):
-    tests=Questions.objects.all()
-    return render(request, 'main/tests.html', {"tests":tests})
-
-
