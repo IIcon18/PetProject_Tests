214f4e3e6b43775b0c69012aff978102a847bb37
fanaenic <korapetr@gmail.com>
2024-06-15 00:49:08 +0300

    account

--- Pupok/main/admin.py
+++ Pupok/main/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Articles
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
-admin.site.register(Articles)
+# Define an inline admin descriptor for Profile model
+# which acts a bit like a singleton
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- Pupok/main/models.py
+++ Pupok/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Articles(models.Model):
@@ -17,3 +18,10 @@ class Articles(models.Model):
         verbose_name_plural = 'Методички'
 
 
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
