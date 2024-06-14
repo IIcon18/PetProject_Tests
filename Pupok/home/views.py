e5672fa43352a0d12a11930e558c2a71e17470d5
fanaenic <korapetr@gmail.com>
2024-06-14 22:15:25 +0300

    test(remastered)

--- Pupok/home/views.py
+++ Pupok/home/views.py
@@ -1,3 +1,49 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponse
+from .models import *
+
+import random
+
+def home(request):
+    context = {'categories': Types.objects.all()}
+    
+    if request.GET.get('gfg'):
+        return redirect(f"quiz/?gfg={request.GET.get('gfg')}")
+    
+    return render(request, 'test/home.html', context)
+
+def quiz(request):
+    context = {'gfg': request.GET.get('gfg')}
+    return render(request, 'test/quiz.html', context)
+
+def get_quiz(request):
+    try:
+        question_objs = Question.objects.all()
+        
+        if request.GET.get('gfg'):
+            question_objs = question_objs.filter(gfg__gfg_name__icontains=request.GET.get('gfg'))
+            
+        question_objs = list(question_objs)
+        data = []
+        random.shuffle(question_objs)
+        
+        for question_obj in question_objs:
+            data.append({
+                "uid": question_obj.uid,
+                "gfg": question_obj.gfg.gfg_name,
+                "question": question_obj.question,
+                "marks": question_obj.marks,
+                "question_type": question_obj.question_type,
+                "answers": question_obj.get_answers(),
+            })
+
+        payload = {'status': True, 'data': data}
+        return JsonResponse(payload)
+        
+    except Exception as e:
+        print(e)
+        return HttpResponse("Something went wrong")
+
 def result(request):
     try:
         if request.method == 'POST':
@@ -57,3 +103,4 @@ def result(request):
     except Exception as e:
         print(f"Exception: {e}")
         return HttpResponse("Something went wrong")
+
