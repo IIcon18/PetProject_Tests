b48f283051355a17c8944c644c85a80b7d09971e
Ilya <145450507+IIcon18@users.noreply.github.com>
2024-06-20 13:49:13 +0300

    Delete Pupok directory

--- Pupok/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.login_view, name='log'),
-    path('', views.logout_view, name='home'),
-]
--- Pupok/tests/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-
-class Choice(models.Model):
-    Question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    is_correct = models.BooleanField(default=False)
