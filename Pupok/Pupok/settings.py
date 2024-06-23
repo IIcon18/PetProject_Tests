6aa38bc3ac4d20d88372963f65448161c7b2ef34
IIcon18 <ogkrit882@gmail.com>
2024-06-23 18:32:00 +0300

    front po krasote

--- Pupok/Pupok/settings.py
+++ Pupok/Pupok/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "main/static",
+]
+
 # settings.py
 import os
 
