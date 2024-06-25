b31df2f646aaf10aefbede232a350760a95b0b76
fanaenic <korapetr@gmail.com>
2024-06-25 16:31:50 +0300

    newmanager

--- Pupok/main/admin.py
+++ Pupok/main/admin.py
@@ -3,48 +3,41 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Profile, Document
 
-# Define an inline admin descriptor for Profile model
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
-    verbose_name_plural = 'Профиль'  # Изменение множественного числа
+    verbose_name_plural = 'Профиль'
+    fields = ('phone_number', 'position', 'patronymic', 'is_manager', 'hire_date', 'last_test_date')  # Включаем новые поля
 
-    # Customize fields displayed in inline
-    fields = ('phone_number', 'position', 'patronymic', 'manager', 'hire_date', 'last_test_date')
-
-# Define a new User admin
 class UserAdmin(BaseUserAdmin):
     inlines = (ProfileInline,)
-    list_display = ('username', 'email', 'first_name', 'last_name', 'get_patronymic', 'get_manager', 'get_position', 'get_hire_date', 'get_last_test_date')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'get_patronymic', 'get_position', 'get_is_manager', 'get_hire_date', 'get_last_test_date')
 
     def get_position(self, instance):
         return instance.profile.position if hasattr(instance, 'profile') else None
-    get_position.short_description = 'Должность'  # Название колонки
-
-    def get_manager(self, instance):
-        return instance.profile.manager if hasattr(instance, 'profile') else None
-    get_manager.short_description = 'Руководитель'  # Название колонки
+    get_position.short_description = 'Должность'
 
     def get_patronymic(self, instance):
         return instance.profile.patronymic if hasattr(instance, 'profile') else None
-    get_patronymic.short_description = 'Отчество'  # Название колонки
+    get_patronymic.short_description = 'Отчество'
+
+    def get_is_manager(self, instance):
+        return instance.profile.is_manager if hasattr(instance, 'profile') else False
+    get_is_manager.short_description = 'Руководитель'
 
     def get_hire_date(self, instance):
         return instance.profile.hire_date if hasattr(instance, 'profile') else None
-    get_hire_date.short_description = 'Дата принятия на работу'  # Название колонки
+    get_hire_date.short_description = 'Дата принятия на работу'
 
     def get_last_test_date(self, instance):
         return instance.profile.last_test_date if hasattr(instance, 'profile') else None
-    get_last_test_date.short_description = 'Последняя дата прохождения теста'  # Название колонки
+    get_last_test_date.short_description = 'Последняя дата прохождения теста'
 
-# Unregister old User admin
-admin.site.unregister(User)
-
-# Register new User admin that includes ProfileInline
-admin.site.register(User, UserAdmin)
-
-# Register Document model
-@admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ('title', 'uploaded_at')
     search_fields = ('title',)
+
+# Регистрация моделей в админке
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Document, DocumentAdmin)
--- Pupok/main/models.py
+++ Pupok/main/models.py
@@ -6,23 +6,27 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     position = models.CharField(max_length=100, blank=True, null=True)
-    patronymic = models.CharField(max_length=100, blank=True, null=True)  # Добавлено поле отчества
-    manager = models.CharField(max_length=100, blank=True, null=True)  # Добавлено поле руководитель
-    hire_date = models.DateField(blank=True, null=True)  # Добавлено поле дата принятия на работу
-    last_test_date = models.DateField(blank=True, null=True)  # Добавлено поле последняя дата прохождения теста
+    patronymic = models.CharField(max_length=100, blank=True, null=True)
+    is_manager = models.BooleanField(default=False)  # Поле "Руководитель"
+    hire_date = models.DateField(blank=True, null=True)  # Дата принятия на работу
+    last_test_date = models.DateField(blank=True, null=True)  # Последняя дата прохождения теста
 
     def __str__(self):
         return self.user.username
 
 def file_upload_path(instance, filename):
     basename, extension = os.path.splitext(filename)
-    truncated_basename = basename[:150]  # Обрезаем имя файла до 100 символов
+    truncated_basename = basename[:150]
     return f'documents/{truncated_basename}{extension}'
 
 class Document(models.Model):
-    title = models.CharField(max_length=150)  # Установите максимальное значение в 200 символов, если нужно
+    title = models.CharField(max_length=150)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to=file_upload_path)
+
+    class Meta:
+        verbose_name = "Методичка"
+        verbose_name_plural = "Методички"
 
     def __str__(self):
         return self.title
